c903470edfd7fae24ce45dedb720de29f1bcf328
e_mon <ovirajemon143@gmail.com>
2020-11-24 02:32:28 +0600

    simple design of circle fun

---
+++ index.js
@@ -0,0 +1,7 @@
+var left_circle = document.querySelector(".left");
+
+function work() {
+    left_circle.classList.remove("left");
+    left_circle.classList.add("left_move");
+}
+left_circle.classList.remove("left_move");
